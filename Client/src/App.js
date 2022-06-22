a43dfd78edb11a266183431d5ff712827799db81
jakecolledge97 <jakecolledgework@gmail.com>
2022-06-22 21:21:52 +0930

    setup back end

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
 import LoginPage from "./LoginPage/LoginPage";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <LoginPage />
+        {data}
       </header>
     </div>
   );
