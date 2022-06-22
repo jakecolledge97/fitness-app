36a26b34a4dab857628adc021776dd9049558a13
jakecolledge97 <jakecolledgework@gmail.com>
2022-06-22 22:32:08 +0930

    implemented heroku

--- Client/src/App.js
+++ Client/src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <LoginPage />
-        {data}
+        {!data ? "Loading..." : data}
       </header>
     </div>
   );
